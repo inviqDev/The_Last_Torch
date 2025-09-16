9ccf48bb7e4f34d2539aaf30ff3c244a61c050bb
inviqDev <inviq.dev@gmail.com>
2025-09-16 16:55:04 +0300

    inviqdev _ inplemented ligtning sound

---
+++ Assets/Scripts/Runtime/Managers/SoundManager.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Runtime
+{
+    public class SoundManager : Singleton<SoundManager>
+    {
+        [SerializeField] private List<Sound> sounds;
+        
+        private Dictionary<string, Sound> _soundDictionary;
+
+        public void Init()
+        {
+            _soundDictionary = new Dictionary<string, Sound>();
+            foreach (var s in sounds)
+            {
+                var key = s.UniquePoolKey;
+                _soundDictionary.Add(key, s);
+            }
+        }
+
+        public void PlaySound(string uniquePoolKey, Vector3 position)
+        {
+            if (!_soundDictionary.TryGetValue(uniquePoolKey, out var s))
+            {
+                UnityEngine.Assertions.Assert.IsNotNull(s, "SoundManager: sound is not found");
+                return;
+            }
+
+            var sound = Pool.Instance?.TryGetObjectFromPool(s);
+            sound?.Play(position);
+        }
+    }
+}
